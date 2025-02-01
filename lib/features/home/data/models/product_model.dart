55bc78934ce3c692a7bb28d086555b75c6b7b4e2
Behruz1308 <behruzergashaliyevv@gmail.com>
2025-02-01 13:46:35 +0500

    first

--- lib/features/home/data/models/product_model.dart
+++ lib/features/home/data/models/product_model.dart
@@ -17,6 +17,8 @@ class ProductModel {
   factory ProductModel.fromJson(Map<String, dynamic> json) =>
       _$ProductModelFromJson(json);
 
+  get data => null;
+
   Map<String, dynamic> toJson() => _$ProductModelToJson(this);
 }
 
